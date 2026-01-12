1ae842d5132f97aea0e87d1f76b946cb2e78a2d7
sitiulawu <sitiulawulandari@gmail.com>
2026-01-12 10:18:11 +0700

    menambah fitur ubah data siswa

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ export async function tampilkanDaftarSiswa() {
     const kolomAksi = document.createElement("td")
     
     //buat tombol edit 
-    const tombolEdit = document.createElement("button")
+    const tombolEdit = document.createElement("a")
     tombolEdit.textContent = "edit"
     tombolEdit.href = "edit.html?id=" + id
     tombolEdit.className = "button edit"
@@ -117,4 +117,25 @@ export async function hapusSiswa(id) {
 
 //refresh daftar siswa
 await tampilkanDaftarSiswa()
+}
+
+//fungsi untuk mengambil data siswa berdasarkan id
+//agar data ditampilkan di form tambah
+export async function ambilDataSiswa(id) {
+  const docRef = doc(db, "siswa", id)
+  const docSnap = await getDoc(docRef)
+  
+  return await docSnap.data()
+}
+
+//fungsi untuk mengubah data siswa
+export async function ubahDataSiswa(id,nis,nama,kelas) { await updateDoc(doc(db, 'siswa', id,), {
+  nis: nis,
+  nama: nama, 
+  kelas: kelas
+  
+})
+    //alihkan ke halaman daftar siswa
+    window.location.herf = "daftar.html"
+  
 }
